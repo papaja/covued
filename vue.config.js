bd56a8a5cfdb1c21c577bc340e7907d58e7ab719
papaja <milan.letic@gmail.com>
2021-03-27 15:32:11 +0100

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/covued/'
+    : '/',
     chainWebpack: (config) => {
         config.module
             .rule("images")
