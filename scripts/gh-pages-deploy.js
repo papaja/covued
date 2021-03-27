5744968d77b72b61ff5c5f0962d74d940423947c
papaja <milan.letic@gmail.com>
2021-03-27 20:18:56 +0100

    -

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -10,6 +10,7 @@ const { Server } = require("http");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await fs.copyFile(folderName+"/index.html", folderName+"/404.html", (err) => {console.log(err)});
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
